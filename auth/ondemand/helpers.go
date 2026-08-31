47bc05bd98f14c7ee19328b0dd8e3ef04877a819
Gonçalo Correia <goncalojose.nevescorreia@gmail.com>
2026-08-31 18:35:20 +0100

    Refactoração Completa no fluxo de Login

--- auth/ondemand/helpers.go
+++ auth/ondemand/helpers.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -18,11 +17,24 @@ import (
 	"time"
 
 	"github.com/chromedp/cdproto/network"
+	"github.com/chromedp/cdproto/target"
 	"github.com/chromedp/chromedp"
+	"github.com/wailsapp/wails/v2/pkg/runtime"
 )
+
+type DevToolsTarget struct {
+	ID                   string `json:"id"`
+	Type                 string `json:"type"`
+	Title                string `json:"title"`
+	Description          string `json:"description"`
+	URL                  string `json:"url"`
+	WebSocketDebuggerUrl string `json:"webSocketDebuggerUrl"`
+}
 
 const SharepointProcessAppID = "sharepoint-app-automation-v1"
 const DebugPort = "9732"
+
+const USER_REQUIRED_ERROR = "USER_INTERACTION_REQUIRED"
 
 func (c *AuthCnfg) cacheCookieToDisk(cookies *Cookies) error {
 	tmpDir := filepath.Join(os.TempDir(), "gosip")
@@ -84,82 +96,175 @@ func (c *AuthCnfg) getCookieDiskCache() (*Cookies, error) {
 
 func (c *AuthCnfg) loadCookies() (*Cookies, error) {
 
+	// Fluxo de login automático
+	foundCookies, err := Login(c.SiteURL, true)
+
+	if err != nil {
+
+		if err.Error() == USER_REQUIRED_ERROR {
+			runtime.EventsEmit(c.Ctx, USER_REQUIRED_ERROR)
+
+			// Fluxo de login manual
+			foundCookies, err = Login(c.SiteURL, false)
+
+			if err != nil {
+				return nil, err
+			}
+
+		} else {
+			return nil, err
+		}
+
+	}
+
+	return foundCookies, nil
+}
+
+func Login(URL string, headless bool) (*Cookies, error) {
 	edge, edgeNotFoundError := getEdgePath()
 
 	if edgeNotFoundError != nil {
 		return nil, edgeNotFoundError
 	}
 
-	userDataDir, err := os.MkdirTemp("", "edge-profile-*")
+	userDataDir := filepath.Join(os.TempDir(), "SharePoint_Edge_Login")
 
-	if err != nil {
-		return nil, fmt.Errorf("criar perfil temporário: %w", err)
-	}
+	cleanUp(userDataDir)
 
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.ExecPath(edge),
-		chromedp.DisableGPU,
-		chromedp.NoSandbox,
-		chromedp.UserDataDir(userDataDir),
-		chromedp.Flag("disable-features", "IsolateOrigins,site-per-process,Translate,BlinkGenPropertyTrees,msImplicitSignin,msEdgeFirstRun,msEdgeFirstRunImplicitSignin,RendererCodeIntegrity"),
-		chromedp.Flag("auth-server-whitelist", "*.sharepoint.com"),
-		chromedp.Flag("sharepoint-id", SharepointProcessAppID),
-		chromedp.Flag("remote-debugging-port", DebugPort),
+	args := append(defaultFlags,
+		fmt.Sprintf("--remote-debugging-port=%s", DebugPort),
+		fmt.Sprintf("--user-data-dir=%s", userDataDir),
+		fmt.Sprintf("--sharepoint-id=%s", SharepointProcessAppID),
+		"--app=data:text/html,",
+		"--window-size=500,600",
+		"--window-position=100,100",
+
+		// DEBUG FLAGS:
+		// "--enable-logging=stderr",
+		// "--v=1",
+		// "--log-level=0",
 	)
 
-	allocCtx, cancelAlloc := chromedp.NewExecAllocator(c.Ctx, opts...)
-	defer cancelAlloc()
+	if headless {
+		args = append(args,
+			"--headless",
+			"--hide-scrollbars",
+			"--mute-audio",
+		)
+	}
 
-	ctx, cancel := chromedp.NewContext(allocCtx)
-	defer cancel()
+	cmd := exec.Command(edge, args...)
 
-	ctx, cancelTimeout := context.WithTimeout(ctx, 5*time.Minute)
-	defer cancelTimeout()
-
-	foundCookies := Cookies{}
-
-	err = runBrowser(ctx, c.SiteURL, &foundCookies)
-
-	defer func() {
-		clearPendingProcess()
-		os.RemoveAll(userDataDir)
-	}()
-
-	if err != nil {
-		if strings.HasSuffix(strings.TrimSpace(err.Error()), "chrome failed to start:") {
-
-			foundCookies = Cookies{}
-
-			err = reconnect(c.Ctx, c.SiteURL, &foundCookies)
-
-			if err != nil {
-				return nil, fmt.Errorf("ERRO_SENTINELONE")
-			}
-
-			return &foundCookies, nil
-		}
-
+	if err := cmd.Start(); err != nil {
 		return nil, err
 	}
 
-	return &foundCookies, nil
-}
+	defer func() {
+		cleanUp(userDataDir)
+	}()
 
-func reconnect(ctx context.Context, url string, foundCookies *Cookies) error {
-	wsURL, wsErr := obterWebSocketURL(DebugPort)
+	devToolsTarget, err := getEdgeWebSocketDebugger()
 
-	if wsErr != nil {
-
-		return fmt.Errorf("Bloqueado pelo EDR e falha na reconexão: %v", wsErr)
+	if err != nil {
+		return nil, err
 	}
 
-	remoteCtx, remoteCancel := chromedp.NewRemoteAllocator(ctx, wsURL)
-	defer remoteCancel()
+	allocCtx, cancel := chromedp.NewRemoteAllocator(context.Background(), devToolsTarget.WebSocketDebuggerUrl)
+	defer cancel()
 
-	remoteBrowserCtx, remoteBrowserCancel := chromedp.NewContext(remoteCtx)
-	defer remoteBrowserCancel()
+	pageCtx, cancelCtx := chromedp.NewContext(allocCtx, chromedp.WithTargetID(target.ID(devToolsTarget.ID)))
+	defer cancelCtx()
 
-	return runBrowser(remoteBrowserCtx, url, foundCookies)
+	ctx, cancelTimeout := context.WithTimeout(pageCtx, 5*time.Minute)
+	defer cancelTimeout()
+
+	var currentURL string
+
+	// AuthURL := "https://www.office.com/login?prompt=select_account&ru=/launch/sharepoint/"
+
+	// _, err = chromedp.RunResponse(ctx,
+	// 	chromedp.Navigate(AuthURL),
+	// 	chromedp.WaitReady("body", chromedp.ByQuery),
+	// 	chromedp.Location(&currentURL),
+	// )
+
+	_, err = chromedp.RunResponse(ctx,
+		chromedp.Navigate(URL),
+		chromedp.WaitReady("body", chromedp.ByQuery),
+		chromedp.Location(&currentURL),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !strings.HasPrefix(currentURL, URL) {
+		if headless {
+			return nil, fmt.Errorf(USER_REQUIRED_ERROR)
+		}
+
+		err = chromedp.Run(ctx,
+			chromedp.ActionFunc(func(ctx context.Context) error {
+				ticker := time.NewTicker(500 * time.Millisecond)
+
+				defer ticker.Stop()
+
+				for {
+					select {
+					case <-ctx.Done():
+						return ctx.Err()
+
+					case <-ticker.C:
+
+						if err := chromedp.Location(&currentURL).Do(ctx); err != nil {
+							return err
+						}
+
+						if strings.HasPrefix(currentURL, URL) {
+							return nil
+						}
+					}
+				}
+			}),
+		)
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	foundCookies := &Cookies{}
+
+	err = chromedp.Run(ctx,
+
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			cookies, err := network.GetCookies().
+				WithURLs([]string{URL}).
+				Do(ctx)
+
+			if err != nil {
+				return err
+			}
+
+			for _, c := range cookies {
+				if slices.Contains(cookieNames, strings.ToLower(c.Name)) {
+					*foundCookies = append(*foundCookies, Cookie{c})
+				}
+			}
+
+			if foundCookies.isEmpty() {
+				return fmt.Errorf("cookies %q not found for %s", cookieNames, URL)
+			}
+
+			return nil
+		}),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return foundCookies, nil
 }
 
 func getEdgePath() (string, error) {
@@ -181,6 +286,51 @@ func getEdgePath() (string, error) {
 
 }
 
+func getEdgeWebSocketDebugger() (*DevToolsTarget, error) {
+	url := fmt.Sprintf("http://127.0.0.1:%s/json/list", DebugPort)
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	for range 3 {
+		resp, err := client.Get(url)
+		if err == nil && resp.StatusCode == http.StatusOK {
+			defer resp.Body.Close()
+
+			var targets []DevToolsTarget
+
+			if err := json.NewDecoder(resp.Body).Decode(&targets); err != nil {
+				return nil, fmt.Errorf("Failed to decode JSON: %v", err)
+			}
+
+			for _, t := range targets {
+				if t.Type == "page" {
+					return &t, nil
+				}
+			}
+		}
+
+		time.Sleep(2 * time.Second)
+	}
+
+	return nil, fmt.Errorf("Não foi possível obter o websocket após 3 tentativas")
+}
+
+func cleanUp(userDataDir string) {
+	clearPendingProcess()
+
+	time.Sleep(200 * time.Millisecond)
+
+	var err error
+
+	for range 5 {
+		err = os.RemoveAll(userDataDir)
+		if err == nil {
+			return
+		}
+
+		time.Sleep(200 * time.Millisecond)
+	}
+}
+
 func clearPendingProcess() {
 	psScript := fmt.Sprintf(`Get-CimInstance Win32_Process -Filter "Name='msedge.exe'" | Where-Object { $_.CommandLine -match "%s" } | Invoke-CimMethod -MethodName Terminate`, SharepointProcessAppID)
 
@@ -193,59 +343,37 @@ func clearPendingProcess() {
 	}
 }
 
-type edgeVersion struct {
-	WebSocketDebuggerURL string `json:"webSocketDebuggerUrl"`
-}
+var defaultFlags = []string{
+	"--no-first-run",
+	"--no-default-browser-check",
 
-func obterWebSocketURL(porta string) (string, error) {
-	url := fmt.Sprintf("http://127.0.0.1:%s/json/version", porta)
-	cliente := &http.Client{Timeout: 2 * time.Second}
+	// DEFAULT CHROMEDP FLAGS
+	"--enable-features=NetworkService,NetworkServiceInProcess",
+	"--disable-features=site-per-process,Translate,BlinkGenPropertyTrees",
+	"--force-color-profile=srgb",
 
-	for range 3 {
-		resp, err := cliente.Get(url)
-		if err == nil && resp.StatusCode == http.StatusOK {
-			defer resp.Body.Close()
-			corpo, _ := io.ReadAll(resp.Body)
+	"--disable-background-networking",
+	"--disable-background-timer-throttling",
+	"--disable-backgrounding-occluded-windows",
+	"--disable-breakpad",
+	"--disable-client-side-phishing-detection",
+	"--disable-default-apps",
+	"--disable-dev-shm-usage",
+	"--disable-extensions",
+	"--disable-hang-monitor",
+	"--disable-ipc-flooding-protection",
+	"--disable-popup-blocking",
+	"--disable-prompt-on-repost",
+	"--disable-renderer-backgrounding",
+	"--disable-sync",
+	"--disable-fre",
 
-			var versao edgeVersion
-			if err := json.Unmarshal(corpo, &versao); err == nil && versao.WebSocketDebuggerURL != "" {
-				return versao.WebSocketDebuggerURL, nil
-			}
-		}
-		time.Sleep(2 * time.Second)
-	}
+	"--metrics-recording-only",
+	"--safebrowsing-disable-auto-update",
+	"--enable-automation",
 
-	return "", fmt.Errorf("não foi possível obter o websocket após 3 tentativas")
-}
+	"--password-store=basic",
+	"--use-mock-keychain",
 
-func runBrowser(ctx context.Context, url string, foundCookies *Cookies) error {
-	return chromedp.Run(ctx,
-		network.Enable(),
-
-		// Open the page so cookies can be created by redirects, JS, Set-Cookie headers, etc.
-		chromedp.Navigate(url),
-		chromedp.WaitReady("body", chromedp.ByQuery),
-
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			cookies, err := network.GetCookies().
-				WithURLs([]string{url}).
-				Do(ctx)
-
-			if err != nil {
-				return err
-			}
-
-			for _, c := range cookies {
-				if slices.Contains(cookieNames, strings.ToLower(c.Name)) {
-					*foundCookies = append(*foundCookies, Cookie{c})
-				}
-			}
-
-			if foundCookies.isEmpty() {
-				return fmt.Errorf("cookies %q not found for %s", cookieNames, url)
-			}
-
-			return nil
-		}),
-	)
+	"--disable-component-update",
 }
